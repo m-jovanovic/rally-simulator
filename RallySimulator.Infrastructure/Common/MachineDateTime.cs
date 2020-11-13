7bc0ab6b3401fd0f7fa9ca6bfa2f18a9da5669ec
Milan Jovanović <milannis.995@gmail.com>
2020-11-13 17:01:58 +0100

    - Added Infrastructure project and IDateTime implementation

---
+++ RallySimulator.Infrastructure/Common/MachineDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RallySimulator.Application.Abstractions.Common;
+
+namespace RallySimulator.Infrastructure.Common
+{
+    /// <summary>
+    /// Represents the current machine date and time.
+    /// </summary>
+    internal sealed class MachineDateTime : IDateTime
+    {
+        /// <inheritdoc />
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
---
+++ RallySimulator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RallySimulator.Application.Abstractions.Common;
+using RallySimulator.Infrastructure.Common;
+
+namespace RallySimulator.Infrastructure
+{
+    /// <summary>
+    /// Contains the extensions method for registering dependencies in the DI framework.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Registers the necessary services with the DI framework.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns>The same service collection.</returns>
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IDateTime, MachineDateTime>();
+
+            return services;
+        }
+    }
+}
